1c546cef5367031d0ba498f79fa8d1f8b9563cac
aydawka <aydan.gasimova2@gmail.com>
2023-07-10 23:54:10 -0700

    fix: restructure menu

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,105 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import {defineComponent, h, ref, Component, Ref} from 'vue'
+import { NIcon, NMenu, NCarousel, NGradientText } from 'naive-ui'
+import type { MenuOption } from 'naive-ui'
+import {
+  BookOutline as BookIcon,
+
+} from '@vicons/ionicons5'
+import {RouterLink} from "vue-router";
+
+function renderIcon (icon: Component) {
+  return () => h(NIcon, null, { default: () => h(icon) })
+}
+
+const menuOptions: MenuOption[] = [
+  {
+    label: () =>
+      h(
+        RouterLink,
+        {
+          to: {
+            name: 'home',
+            params: {
+              lang: 'en-US'
+            }
+          }
+        },
+        { default: () => 'Home' }
+      ),
+    key: 'home',
+    icon: renderIcon(BookIcon),
+  },
+      {
+        label: () =>
+      h(
+        RouterLink,
+        {
+          to: {
+            name: 'about-me',
+            params: {
+              lang: 'en-US'
+            }
+          }
+        },
+        { default: () => 'About Me' }
+      ),
+    key: 'about-me',
+    icon: renderIcon(BookIcon),
+  },
+  {
+        label: () =>
+      h(
+        RouterLink,
+        {
+          to: {
+            name: 'projects',
+            params: {
+              lang: 'en-US'
+            }
+          }
+        },
+        { default: () => 'Recent Projects' }
+      ),
+    key: 'projects',
+    icon: renderIcon(BookIcon),
+    children: [
+      {
+        label: 'Cybersecurity',
+        key: 'cyber-security'
+      }
+    ]
+  },
+]
+
+const activeKey:Ref<string>= ref('');
+
+
 </script>
 
 <template>
+      <main class="home-page">
+    <div class="header">
+      <div class="menu">
+          <n-menu v-model:value="activeKey" mode="horizontal" :options="menuOptions" />
+      </div>
+    </div>
+   </main>
   <RouterView />
 </template>
+<style scoped>
 
+.menu{
+    display: flex;
+    justify-content: flex-end;
+    align-items: flex-end;
+
+}
+
+.header{
+    margin: 0 5rem 0 5rem;
+}
+
+h1{margin: 0}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,78 +1,4 @@
 <script setup lang="ts">
-import {defineComponent, h, ref, Component, Ref} from 'vue'
-import { NIcon, NMenu, NCarousel, NGradientText } from 'naive-ui'
-import type { MenuOption } from 'naive-ui'
-import {
-  BookOutline as BookIcon,
-
-} from '@vicons/ionicons5'
-import {RouterLink} from "vue-router";
-
-function renderIcon (icon: Component) {
-  return () => h(NIcon, null, { default: () => h(icon) })
-}
-
-const menuOptions: MenuOption[] = [
-  {
-    label: () =>
-      h(
-        RouterLink,
-        {
-          to: {
-            name: 'home',
-            params: {
-              lang: 'en-US'
-            }
-          }
-        },
-        { default: () => 'Home' }
-      ),
-    key: 'home',
-    icon: renderIcon(BookIcon),
-  },
-      {
-        label: () =>
-      h(
-        RouterLink,
-        {
-          to: {
-            name: 'about-me',
-            params: {
-              lang: 'en-US'
-            }
-          }
-        },
-        { default: () => 'About Me' }
-      ),
-    key: 'about-me',
-    icon: renderIcon(BookIcon),
-  },
-  {
-        label: () =>
-      h(
-        RouterLink,
-        {
-          to: {
-            name: 'projects',
-            params: {
-              lang: 'en-US'
-            }
-          }
-        },
-        { default: () => 'Recent Projects' }
-      ),
-    key: 'projects',
-    icon: renderIcon(BookIcon),
-    children: [
-      {
-        label: 'Cybersecurity',
-        key: 'cyber-security'
-      }
-    ]
-  },
-]
-
-const activeKey:Ref<string>= ref('');
 
 </script>
 
@@ -80,9 +6,6 @@ const activeKey:Ref<string>= ref('');
   <main class="home-page">
     <div class="header">
       <h1><a href="/">home</a></h1>
-          <div class="menu">
-              <n-menu v-model:value="activeKey" mode="horizontal" :options="menuOptions" />
-          </div>
     </div>
 <div class="carousel">
 
@@ -128,12 +51,6 @@ const activeKey:Ref<string>= ref('');
   object-fit: cover;
 }
 
-.menu{
-    display: flex;
-    justify-content: flex-end;
-    align-items: flex-end;
-
-}
 
 .carousel-text {
   position: absolute;
