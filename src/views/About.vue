5329f2cc0b4e594fbce67d7b3e2fb757d12b601a
aydawka <aydan.gasimova2@gmail.com>
2023-07-12 00:36:04 -0700

    feat: about and projects were split up

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,52 +1,9 @@
 <script setup lang="ts">
-import {NList, NListItem, NTag, NSpace, NThing} from 'naive-ui'
-
-
 
 </script>
 <template>
-  <n-list hoverable clickable>
-    <n-list-item>
-      <n-thing title="Better Late Than Never" content-style="margin-top: 10px;">
-        <template #description>
-          <n-space size="small" style="margin-top: 4px">
-            <n-tag :bordered="false" type="info" size="small">
-              Tag A
-            </n-tag>
-            <n-tag :bordered="false" type="info" size="small">
-              Tag B
-            </n-tag>
-          </n-space>
-        </template>
-        Lorem ipsum dolor sit amet,<br>
-        consectetur adipiscing elit,<br>
-        sed do eiusmod tempor incididunt<br>
-        ut labore et dolore magna aliqua.<br>
-        Ut enim ad minim veniam,<br>
-        quis nostrud exercitation ullamco
-      </n-thing>
-    </n-list-item>
-    <n-list-item>
-      <n-thing title="Lorem Ipsum" content-style="margin-top: 10px;">
-        <template #description>
-          <n-space size="small" style="margin-top: 4px">
-            <n-tag :bordered="false" type="info" size="small">
-              Tag C
-            </n-tag>
-            <n-tag :bordered="false" type="info" size="small">
-              Tag D
-            </n-tag>
-          </n-space>
-        </template>
-        Lorem ipsum dolor sit amet,<br>
-        consectetur adipiscing elit,<br>
-        sed do eiusmod tempor incididunt<br>
-        ut labore et dolore magna aliqua.<br>
-        Ut enim ad minim veniam,<br>
-        quis nostrud exercitation ullamco
-      </n-thing>
-    </n-list-item>
-  </n-list>
+
+  <h1>About page</h1>
 </template>
 
 <style>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,11 +1,53 @@
+<script setup>
+import {NList, NListItem, NSpace, NTag, NThing} from "naive-ui";
 
+
+</script>
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <h1>Projects</h1>
+  <n-list hoverable clickable>
+    <n-list-item>
+      <n-thing title="Better Late Than Never" content-style="margin-top: 10px;">
+        <template #description>
+          <n-space size="small" style="margin-top: 4px">
+            <n-tag :bordered="false" type="info" size="small">
+              Tag A
+            </n-tag>
+            <n-tag :bordered="false" type="info" size="small">
+              Tag B
+            </n-tag>
+          </n-space>
+        </template>
+        Lorem ipsum dolor sit amet,<br>
+        consectetur adipiscing elit,<br>
+        sed do eiusmod tempor incididunt<br>
+        ut labore et dolore magna aliqua.<br>
+        Ut enim ad minim veniam,<br>
+        quis nostrud exercitation ullamco
+      </n-thing>
+    </n-list-item>
+    <n-list-item>
+      <n-thing title="Lorem Ipsum" content-style="margin-top: 10px;">
+        <template #description>
+          <n-space size="small" style="margin-top: 4px">
+            <n-tag :bordered="false" type="info" size="small">
+              Tag C
+            </n-tag>
+            <n-tag :bordered="false" type="info" size="small">
+              Tag D
+            </n-tag>
+          </n-space>
+        </template>
+        Lorem ipsum dolor sit amet,<br>
+        consectetur adipiscing elit,<br>
+        sed do eiusmod tempor incididunt<br>
+        ut labore et dolore magna aliqua.<br>
+        Ut enim ad minim veniam,<br>
+        quis nostrud exercitation ullamco
+      </n-thing>
+    </n-list-item>
+  </n-list>
 </template>
 
 <style>
 </style>
-<script setup>
-</script>
