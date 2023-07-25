9eb7c06e3b4e46ee541144de83db794c28091dc7
aydawka <aydan.gasimova2@gmail.com>
2023-07-25 00:49:27 -0700

    feat: added animations

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,24 +1,53 @@
 <script setup lang="ts">
+import {NCard } from 'naive-ui'
+import DocumentationPageLottie from "@/assets/animation_lkhzpapw.json";
+import { Vue3Lottie } from "vue3-lottie";
 
 </script>
 <template>
 
   <h1>About page</h1>
-
+    <div class="mb-8">
+      <Vue3Lottie :animationData="DocumentationPageLottie" :height="300" :width="300" />
+    </div>
   <div class="about">
 
-      Hello! I'm a software engineer with practical experience developing applications using JavaScript and Python.
+    <n-card hoverable title="About me">
+        Hello! I'm a software engineer with practical experience developing applications using JavaScript and Python.
           I'm a passionate, driven, and quick life-long learner dedicated to providing solutions through problem solving.
-      My ORCID si https://orcid.org/0009-0001-6902-097X
-      List of needs:
-      my publications
-      orcid id
-      google scholar page
-      research gate
-
-      linkedin
+    </n-card>
+    <n-card title="My ORCID">
+        https://orcid.org/0009-0001-6902-097X
+    </n-card>
+    <n-card title="My github address">
+        <a href="https://scholar.google.com/citations?user=7n1nsmsAAAAJ&hl=en&oi=ao" target="_blank">My github</a>
+    </n-card>
+    <n-card title="My scholar page address">
+        <a href="https://scholar.google.com/citations?user=7n1nsmsAAAAJ&hl=en&oi=ao" target="_blank">My scholar</a>
+    </n-card>
+    <n-card title="My linkedin">
+        <a href="https://scholar.google.com/citations?user=7n1nsmsAAAAJ&hl=en&oi=ao" target="_blank">My linkedin</a>
+    </n-card>
+    <n-card title="My researchgate">
+        <a href="https://scholar.google.com/citations?user=7n1nsmsAAAAJ&hl=en&oi=ao" target="_blank">My researchgate</a>
+    </n-card>
 
   </div>
 </template>
-<style>
+<style scoped>
+
+.about{display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+.about .n-card.n-card--bordered{
+    max-width: 700px;
+
+}
+
+h1{
+    justify-content: center;
+    display: flex;
+}
 </style>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,21 +1,22 @@
 <script setup>
 import {NList, NListItem, NCard, NThing} from "naive-ui";
+import { Icon, IconConfigProvider } from "@vicons/utils";
 
 
 </script>
 <template>
-  <h1>Projects</h1>
+  <h1>Projects <img src="../assets/FundProjectionScreenOutlined.svg" style="width: 40px;height: 40px"></h1>
     <div class="project-list">
   <n-list hoverable clickable>
     <n-list-item>
-      <n-card title="Better Late Than Never" content-style="margin-top: 10px;">
-        Lorem ipsum dolor sit amet,<br>
-        consectetur adipiscing elit,<br>
+      <n-card title="Summer conference system"
+              content-style="margin-top: 10px;">
+        conference system<br>
 
       </n-card>
     </n-list-item>
     <n-list-item>
-      <n-card title="Lorem Ipsum" content-style="margin-top: 10px;">
+      <n-card title="Web crawler" content-style="margin-top: 10px;">
         Ut enim ad minim veniam,<br>
         quis nostrud exercitation ullamco
       </n-card>
