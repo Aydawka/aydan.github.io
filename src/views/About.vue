ff7a189a382eb1481fb3b929fa43cc3620989c82
aydawka <aydan.gasimova2@gmail.com>
2023-08-02 00:15:44 -0700

    fix: style fixed

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,20 +4,19 @@ import { Vue3Lottie } from "vue3-lottie";
 
 </script>
 <template>
-
-  <h1 class="about-h1">About Page</h1>
     <div class="about-general">
       <div class="about">
-         Hello! I'm a software engineer with practical experience developing applications using JavaScript and Python.
+         <div>
+           <h1 class="about-h1">About Page</h1>
+         </div>
+
+          Hello! I'm a software engineer with practical experience developing applications using JavaScript and Python.
         I'm a passionate, driven, and quick life-long learner dedicated to providing solutions through problem solving and troubleshooting.
         I am a creative soul who loves brainstorming solutions – be it with a computer or a client.
         I love all aspects of software development from full-stack development to data science.
         Although I enjoy experimenting with different technology and building side-projects,
         I excel in a collaborative team-based environment with a variety of differing perspectives.
-        I have successfully built clean, scalable, and dynamic front-end web applications with TypeScript and Angular.
-        Using Python I have developed Flask-based web applications and machine learning techniques for
-        cybersecurity threat detection using neural networks.
-        Some of my skills include:
+
 
       </div>
       <div class="yoga">
@@ -27,7 +26,8 @@ import { Vue3Lottie } from "vue3-lottie";
 </template>
 <style scoped>
 
-.about{display: flex;
+.about{
+  display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
@@ -35,10 +35,35 @@ import { Vue3Lottie } from "vue3-lottie";
     font-size: 1rem;
     font-weight: bold;
     color: #3b1f29;
-    margin-left: 30rem;
-
+    margin-left: 15rem;
     padding: 5rem;
+    width: 900px;
+    height: 400px;
+    box-shadow: inset 20px 20px 20px rgba(0,0,0,0.05),
+    25px 35px 20px rgba(0,0,0,0.05),
+    25px 35px 30px rgba(0,0,0,0.05),
+    inset -20px -20px 25px rgba(0,0,0,0.09);
+    border-radius: 57% 43% 43% 57%/43% 43% 57% 57%;
+  transition: 0.5s ease-in-out;
 }
+
+.about:hover{
+  border-radius: 50%;
+}
+
+.about::before{
+  content: '';
+  position: absolute;
+  width: 35px;
+  height: 35px;
+  top: 50px;
+  left: 85px;
+  background: #fff;
+  border-radius: 50%;
+  opacity: 0.9;
+}
+
+
 
 .about .n-card.n-card--bordered {
     max-width: 700px;
@@ -49,14 +74,14 @@ import { Vue3Lottie } from "vue3-lottie";
 
 .about-h1{
     font-size: 4rem;
-    transform: translateY(50%);
+    margin-top: 0!important;
     font-weight: 800;
-    justify-content: center;
-    display: flex;
-  margin-left: -2rem;
+
+
 }
 
-.about-general{display: flex;}
+.about-general{display: flex;
+    margin-top: 6rem;}
 
 .yoga{min-width: 35rem}
 </style>
