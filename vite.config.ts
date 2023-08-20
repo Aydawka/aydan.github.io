0335b04a16ea1d7cec4b1ac88d6e005bb0a4ee36
aydawka <aydan.gasimova2@gmail.com>
2023-08-20 12:22:49 -0700

    fix: on deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,27 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+ plugins: [
+        vue(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
+        extensions: [
+            '.js',
+            '.json',
+            '.jsx',
+            '.mjs',
+            '.ts',
+            '.tsx',
+            '.vue',
+        ],
+    },
+    server: {
+        port: 3000,
+    },
+    base: './',
 })
+
+
